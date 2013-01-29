b15900b271eac19bec862981b663179b5d2b71ae
Joris Vercammen <joris@dichtlog.nl>
2013-01-29 20:32:17 +0100

    fixed error in /projectdetail

--- htdocs/src/Botchla/JiraBundle/Controller/DefaultController.php
+++ htdocs/src/Botchla/JiraBundle/Controller/DefaultController.php
@@ -16,20 +16,24 @@ class DefaultController extends Controller
 
     public function __construct()
     {
+        if (file_exists('/vagrant/testfile.php')) {
+          include '/vagrant/testfile.php';
+          $this->JIRA_URL = $jira_url;
+          $this->LOGIN    = $jira_login;
+          $this->PASSWORD = $jira_pass;
+        }
 
-        if (!isset($JIRA_URL)) {
+        if ($this->JIRA_URL == '') {
             throw $this->createNotFoundException('JIRA url is not set');
         }
     }
 
     /**
-     * @Route("project")
+     * @Route("/project")
      * @Template("BotchlaJiraBundle:Projects:index.html.twig")
      */
     public function projectAction()
     {
-
-        // start CURL action
         $projects = $this->doCurl($this->JIRA_URL.'project');
 
         return array(
@@ -39,14 +43,13 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/issue/{projectAbbreviation}")
+     * @Route("/project/{abbr}")
      * @Template("BotchlaJiraBundle:Issues:issues.html.twig")
      */
-    public function issueAction($projectAbbreviation)
+    public function projectDetailAction($abbr = null)
     {
-        // start CURL action
-        $issues = $this->doCurl($this->JIRA_URL.'search?jql=project="'.$projectAbbreviation.'"+AND+assignee="'.$this->LOGIN.'"');
-        print_r($issues);
+        $issues = $this->doCurl($this->JIRA_URL.'search?jql=project="'.$abbr.'"+AND+assignee="'.$this->LOGIN.'"');
+
         return array(
           'name' => 'Jira-test',
           'result' => $issues->issues
@@ -61,20 +64,40 @@ class DefaultController extends Controller
     public function todayAction()
     {
 
-        $issues = $this->doCurl($this->JIRA_URL.'search?jql=assignee="'.$this->LOGIN.'"+AND+updatedDate>startOfDay()+AND+updatedDate<endOfDay()');
+        $issues = $this->doCurl($this->JIRA_URL.'search?jql=updatedDate>startOfDay()+AND+updatedDate<endOfDay()+AND+(status+changed+by+"'.$this->LOGIN.'"+OR+assignee="'.$this->LOGIN.'")');
 
+        $totaltime = 0;
         $output_issues = array();
-        foreach( $issues->issues as $k => $issue_list) {
-            // generate url
-            $worklogurl = $this->JIRA_URL.'issue/'.$issue_list->key.'/worklog';
+        if (count($issues) > 0) {
+            foreach( $issues->issues as $k => $issue_list) {
+                // generate url
+                $worklogurl = $this->JIRA_URL.'issue/'.$issue_list->key.'/worklog';
+                $worklog    = $this->doCurl($worklogurl);
 
-            $output_issues[$issue_list->key] = $issue_list;
-            $output_issues[$issue_list->key]->IssueWorklog = $this->doCurl($worklogurl);
+                $output_issues[$issue_list->key] = $issue_list;
+
+                // loop over worklogs
+                if ( isset($worklog->worklogs) && count($worklog->worklogs) > 0 ) {
+                    foreach ($worklog->worklogs as $value) {
+                        // only add worklog to output if this also happened today
+                        if (date('dmY' , strtotime($value->updated)) == date('dmY') ) {
+                            $output_issues[$issue_list->key]->IssueWorklog[] = $value;
+                            $totaltime += $value->timeSpentSeconds;
+                        }
+                    }
+                }
+                if (!isset($output_issues[$issue_list->key]->IssueWorklog)) {
+                    $output_issues[$issue_list->key]->IssueWorklog = null;
+                }
+            }
         }
+
+        $totaltimeFormatted = $this->formatTime($totaltime);
 
         return array(
           'name' => 'Jira-test',
-          'result' => $output_issues
+          'result' => $output_issues,
+          'totaltime' => $totaltimeFormatted
         );
     }
 
@@ -101,5 +124,20 @@ class DefaultController extends Controller
         $result_object = json_decode($result);
         curl_close($curl);
         return $result_object;
+    }
+
+    /**
+     * private function to format time (seconds to human readable)
+     * @param  int      $seconds    seconds
+     * @return string               formatted version
+     */
+    private function formatTime($seconds) {
+        $hours = 0;
+        $mins = 0;
+
+        $hours = floor($seconds / 3600);
+        $mins  = floor(($seconds - ($hours*3600)) / 60);
+
+        return $hours.':'.$mins;
     }
 }
